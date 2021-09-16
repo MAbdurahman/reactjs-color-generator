356a302e5c817fc18f673dc5bb19f109db510dd5
MAbdurahman <mdbdrrhm@gmail.com>
2021-09-16 04:25:27 -0500

    Add some functionality to app

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Values from 'values.js';
+import SingleColor from './../components/colors/SingleColor';
 
 export default function App() {
-   return (
-      <div>
-         <h1>This is App</h1>
-      </div>
-   )
+	//**************** variables ****************//
+	const [color, setColor] = useState('');
+	const [error, setError] = useState(false);
+	const [list, setList] = useState(new Values('#f15025').all(10));
+	//**************** functions ****************//
+	const handleSubmit = e => {
+		e.preventDefault();
+		try {
+			let colors = new Values(color).all(10);
+			setList(colors);
+		} catch (error) {
+			setError(true);
+			console.log(error);
+		}
+	};
+	return (
+		<>
+			<section className='container'>
+				<h3>color generator</h3>
+				<form onSubmit={handleSubmit}>
+					<input
+						type='text'
+						value={color}
+						onChange={e => setColor(e.target.value)}
+						placeholder='#f15025'
+						className={`${error ? 'error' : null}`}
+					/>
+					<button className='btn' type='submit'>
+						submit
+					</button>
+				</form>
+			</section>
+			<section className='colors'>
+				{list.map((color, index) => {
+					return (
+						<SingleColor
+							key={index}
+							{...color}
+							index={index}
+							hexColor={color.hex}
+						/>
+					);
+				})}
+			</section>
+		</>
+	);
 }
